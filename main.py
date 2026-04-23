8180442e19e41d0685d279b9dba4da6534d66b80
Nedha mariyam azim <nedhamariyam494@gmail.com>
2026-04-23 22:31:30 +0530

    Refactor game loop and collision handling

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #initiate pygame
 import pygame
-import time
 import random
-import pygame.display
 pygame.init()
 #set the screen and background
 screen=pygame.display.set_mode((800,600))
@@ -12,59 +10,60 @@ background=pygame.transform.scale(background,(800,600))
 #load the image of the player and transform the size
 player=pygame.image.load('assets/char1.png')
 player=pygame.transform.scale(player,(130,130))
+font1 = pygame.font.Font(None, 100)
+font = pygame.font.Font(None, 40)
 #make the loop for pygame to keep repeating the screen+then add the option to close the loop
 running=True
 x = 0
 y = 383
-ox=0
+ox=400
 oy=0
 a=0
+gameloop = 'playing'
 while running:
-    for i in pygame.event.get():
-        if i.type==pygame.QUIT:
-            running=False
-    screen.blit(background, (0, 0))
-#pygame.key.get_pressed--gives a list of boolean values eg-[True,False,True] and pygame.k_const
-#is the index for which it acesses the list.
-    key=pygame.key.get_pressed()
-    if key[pygame.K_LEFT]:
-        x-=5
-#dont try to change key[pygame.K_const] its an unchangable value we can only control the movement
-    if x <= 0:
-        x = 0
-    if key[pygame.K_RIGHT]:
-        x+=5
-    if x>=680:
-        x=680
-    screen.blit(player,(x,y))
-    oy+=8
-    pygame.draw.rect(screen,'purple',(ox,oy,40,40))
-    pygame.display.update()
-    font=pygame.font.Font(None,40)
-    if oy>600:
-        ox= random.randint(0, 800)
-        oy=0
-        a+=1
-    text1 = font.render('score:' + str(a), True, 'red')
-    screen.blit(text1, (0, 0))
-    pygame.display.update()
-    ro=pygame.Rect(ox,oy,20,20)#rect should be 'Rect' V.IMP
-    rp=pygame.Rect(x,y,100,100)
-    gameloop=False
-    font1=pygame.font.Font(None,100)
-    if rp.colliderect(ro):
-        gameloop=True
-    if gameloop==True:
-        text=font1.render('game over',False,'purple')
-        text2=font.render('press R to restart',True,'red')
-        screen.blit(text2,(255,300))
-        screen.blit(text,(200,240))
+        for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                        running = False
+                if i.type==pygame.KEYDOWN:
+                        if gameloop=='game over':
+                                if i.key == pygame.K_r:
+                                        gameloop = 'playing'
+                                        x = 0
+                                        y = 383
+                                        ox = 400
+                                        oy = 0
+                                        a = 0
+        screen.blit(background, (0, 0))
+        # pygame.key.get_pressed--gives a list of boolean values eg-[True,False,True] and pygame.k_const
+        # is the index for which it acesses the list.
+        if gameloop == 'playing':
+                key = pygame.key.get_pressed()
+                if key[pygame.K_LEFT]:
+                        x -= 5
+                # dont try to change key[pygame.K_const] its an unchangable value we can only control the movement
+                if key[pygame.K_RIGHT]:
+                        x += 5
+                x = min(670, max(0, x))
+                screen.blit(player, (x, y))
+                oy += 8
+                pygame.draw.rect(screen, 'purple', (ox, oy, 40, 40))
+                if oy > 600:
+                        ox = random.randint(0, 760)
+                        oy = 0
+                        a += 1
+                text1 = font.render('score:' + str(a), True, 'red')
+                screen.blit(text1, (0, 0))
+                ro = pygame.Rect(ox, oy, 40, 40)  # rect should be 'Rect' V.IMP
+                rp = pygame.Rect(x, y, 100, 100)
+                if rp.colliderect(ro):
+                        gameloop = 'game over'
+        elif gameloop == 'game over':
+                text = font1.render('game over', True, 'purple')
+                text2 = font.render('press R to restart', True, 'red')
+                screen.blit(text2, (255, 300))
+                screen.blit(text, (200, 240))
         pygame.display.update()
-        time.sleep(5)
-        running=False
 pygame.quit()
-
-
 
 
 
